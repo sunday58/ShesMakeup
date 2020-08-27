b34476871ad71063ba914464f86cd9d7fb02cbb5
sundaydavid <sundaydavid989@gmail.com>
2020-08-27 09:48:26 +0100

    added splash screen

--- app/src/main/java/com/sundaydavid989/shesmakeup/splash/SplashActivity.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/splash/SplashActivity.kt
@@ -1,10 +1,15 @@
 package com.sundaydavid989.shesmakeup.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sundaydavid989.shesmakeup.MainActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
