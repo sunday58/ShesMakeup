c1ebd1a0e402104914e5822725278c6762d3fc03
sundaydavid <sundaydavid989@gmail.com>
2020-08-30 05:09:54 +0100

    finished implementing repository

--- app/src/main/java/com/sundaydavid989/shesmakeup/data/repository/MakeupRepositoryImpl.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/data/repository/MakeupRepositoryImpl.kt
@@ -1,10 +1,36 @@
 package com.sundaydavid989.shesmakeup.data.repository
 
 import androidx.lifecycle.LiveData
+import com.sundaydavid989.shesmakeup.data.db.MakeupDao
 import com.sundaydavid989.shesmakeup.data.db.entity.Makeup
+import com.sundaydavid989.shesmakeup.data.network.MakeupNetworkDataSource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
 
-class MakeupRepositoryImpl : MakeupRepository {
+class MakeupRepositoryImpl(
+    private val makeupDao: MakeupDao,
+    private val makeupNetworkDataSource: MakeupNetworkDataSource
+) : MakeupRepository {
+
+    init {
+        makeupNetworkDataSource.downloadMakeup.observeForever { makeup ->
+            //persist makeups
+            persistFetchedMakeup(makeup)
+        }
+    }
+
     override suspend fun getMakeup(): LiveData<Makeup> {
-        TODO("Not yet implemented")
+        return withContext(Dispatchers.IO) {
+            return@withContext makeupDao.getMakeup()
+        }
+    }
+
+    private fun persistFetchedMakeup(fetchedMakeup: Makeup) {
+        GlobalScope.launch(Dispatchers.IO) {
+            makeupDao.upsert(fetchedMakeup)
+        }
     }
 }
