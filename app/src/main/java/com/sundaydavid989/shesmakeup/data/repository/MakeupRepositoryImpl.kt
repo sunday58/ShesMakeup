190b67589ae7e6152387bf1df347c4eb27d42b90
sundaydavid <sundaydavid989@gmail.com>
2020-08-30 04:51:27 +0100

    implementing repository

--- app/src/main/java/com/sundaydavid989/shesmakeup/data/repository/MakeupRepositoryImpl.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/data/repository/MakeupRepositoryImpl.kt
@@ -3,6 +3,8 @@ package com.sundaydavid989.shesmakeup.data.repository
 import androidx.lifecycle.LiveData
 import com.sundaydavid989.shesmakeup.data.db.entity.Makeup
 
-interface MakeupRepository {
-    suspend fun getMakeup(): LiveData<Makeup>
+class MakeupRepositoryImpl : MakeupRepository {
+    override suspend fun getMakeup(): LiveData<Makeup> {
+        TODO("Not yet implemented")
+    }
 }
