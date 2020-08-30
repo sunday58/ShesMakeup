94bba317f6b78016317d5125bc4f0c275366fe64
sundaydavid <sundaydavid989@gmail.com>
2020-08-30 05:24:28 +0100

    finished setting up Home model factory

--- app/src/main/java/com/sundaydavid989/shesmakeup/ui/home/HomeViewModelFactory.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/home/HomeViewModelFactory.kt
@@ -1,4 +1,14 @@
 package com.sundaydavid989.shesmakeup.ui.home
 
-class HomeViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.sundaydavid989.shesmakeup.data.repository.MakeupRepository
+
+@Suppress("UNCHECKED_CAST")
+class HomeViewModelFactory(
+    private val makeupRepository: MakeupRepository
+): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return HomeViewModel(makeupRepository) as T
+    }
 }
