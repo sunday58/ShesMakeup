82cd9b51c92a6f4ef13a54ecc25d4ff9cbf6db46
sundaydavid <sundaydavid989@gmail.com>
2020-08-30 05:21:52 +0100

    implemented home viewmodel

--- app/src/main/java/com/sundaydavid989/shesmakeup/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/home/HomeViewModel.kt
@@ -1,7 +1,14 @@
 package com.sundaydavid989.shesmakeup.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.sundaydavid989.shesmakeup.data.repository.MakeupRepository
+import com.sundaydavid989.shesmakeup.internal.lazyDeferred
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class HomeViewModel(
+    private val makeupRepository: MakeupRepository
+) : ViewModel() {
+
+    val makeup by lazyDeferred {
+        makeupRepository.getMakeup()
+    }
 }
