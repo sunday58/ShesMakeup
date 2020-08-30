ce5b81f03db20c7f05bcd8fb45c6648fe5fb095c
sundaydavid <sundaydavid989@gmail.com>
2020-08-30 04:35:44 +0100

    finished setting up database

--- app/src/main/java/com/sundaydavid989/shesmakeup/data/db/MakeupDatabase.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/data/db/MakeupDatabase.kt
@@ -1,7 +1,12 @@
 package com.sundaydavid989.shesmakeup.data.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.sundaydavid989.shesmakeup.data.db.entity.MakeupItem
+import com.sundaydavid989.shesmakeup.data.db.entity.MakeupTypeConverter
 
 
 @Database(
@@ -9,6 +14,22 @@ import com.sundaydavid989.shesmakeup.data.db.entity.MakeupItem
     entities = [MakeupItem::class],
     version = 1
 )
-abstract class MakeupDatabase {
+@TypeConverters(MakeupTypeConverter::class)
+abstract class MakeupDatabase : RoomDatabase() {
+        abstract fun makeupItem(): MakeupItem
 
+    companion object {
+        @Volatile private var instance: MakeupDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: buildDatabase(context).also { instance = it}
+        }
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+            MakeupDatabase::class.java,
+            "makeup.db")
+                .fallbackToDestructiveMigration()
+                .build()
+    }
 }
