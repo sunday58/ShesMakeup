9ddb8ae135de753b7cfe178e846d75f7547a01f4
sundaydavid <sundaydavid989@gmail.com>
2020-09-29 09:48:25 +0100

    added Remotekey data class

---
+++ app/src/main/java/com/sundaydavid989/shesmakeup/data/db/entity/RemoteKeys.kt
@@ -0,0 +1,13 @@
+package com.sundaydavid989.shesmakeup.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "remote_keys")
+data class RemoteKeys(
+    @PrimaryKey
+    val id: Int,
+    val prevKey: Int?,
+    val nextKey: Int?
+)
