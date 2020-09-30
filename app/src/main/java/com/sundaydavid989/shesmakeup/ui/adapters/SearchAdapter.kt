43669aa919f26885ce86321f391ede4f7e275dc8
sundaydavid <sundaydavid989@gmail.com>
2020-09-30 01:24:28 +0100

    initializing search fragment

---
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/adapters/SearchAdapter.kt
@@ -0,0 +1,41 @@
+package com.sundaydavid989.shesmakeup.ui.adapters
+
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.sundaydavid989.shesmakeup.R
+import com.sundaydavid989.shesmakeup.data.db.entity.MakeupItem
+
+class SearchAdapter()
+    :PagingDataAdapter<MakeupItem, SearchViewHolder>(REPO_COMPARATOR) {
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        val makeupItem = getItem(position)
+        if (makeupItem != null){
+            holder.bind(makeupItem)
+
+            holder.itemView.setOnClickListener {
+                val bundle = Bundle()
+                bundle.putSerializable("makeups", makeupItem)
+                Navigation.findNavController(holder.itemView).navigate(R.id.itemDetailFragment, bundle)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        return SearchViewHolder.create(parent)
+    }
+
+    companion object {
+        private val REPO_COMPARATOR = object : DiffUtil.ItemCallback<MakeupItem>() {
+            override fun areItemsTheSame(oldItem: MakeupItem, newItem: MakeupItem): Boolean =
+                oldItem.name == newItem.name
+
+            override fun areContentsTheSame(oldItem: MakeupItem, newItem: MakeupItem): Boolean =
+                oldItem == newItem
+        }
+    }
+
+}
--- app/src/main/java/com/sundaydavid989/shesmakeup/ui/search/SearchFragment.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/search/SearchFragment.kt
@@ -2,28 +2,95 @@ package com.sundaydavid989.shesmakeup.ui.search
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import com.sundaydavid989.shesmakeup.R
+import com.sundaydavid989.shesmakeup.databinding.SearchFragmentBinding
+import com.sundaydavid989.shesmakeup.ui.adapters.SearchAdapter
+import com.sundaydavid989.shesmakeup.ui.base.ScopedFragment
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.distinctUntilChangedBy
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.launch
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.closestKodein
+import org.kodein.di.generic.instance
 
-class SearchFragment : Fragment() {
+@ExperimentalCoroutinesApi
+@FlowPreview
+class SearchFragment : ScopedFragment(), KodeinAware {
 
+    override val kodein by closestKodein()
+    private val viewModelFactory: SearchViewModelFactory by instance()
     private lateinit var viewModel: SearchViewModel
+    private var _binding: SearchFragmentBinding? = null
+    private val binding get() = _binding
+    private val adapter = SearchAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.search_fragment, container, false)
+       _binding = SearchFragmentBinding.inflate(inflater, container, false)
+
+
+        return binding?.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SearchViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this, viewModelFactory).get(SearchViewModel::class.java)
+    }
+
+    private fun initSearch(query: String) {
+        binding!!.searchMakeup.setText(query)
+
+        binding!!.searchMakeup.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                updateSearchListFromInput()
+                true
+            }else {
+                false
+            }
+        }
+        binding!!.searchMakeup.setOnKeyListener { _, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode ==
+                    KeyEvent.KEYCODE_ENTER) {
+                updateSearchListFromInput()
+                true
+            }else {
+                false
+            }
+        }
+        lifecycleScope.launch {
+            adapter.loadStateFlow
+                .distinctUntilChangedBy { it.refresh }
+                .filter { it.refresh is LoadState.NotLoading }
+                .collect { binding!!.searchRecyclerView.scrollToPosition(0) }
+        }
+    }
+
+    private fun updateSearchListFromInput() {
+        binding!!.searchMakeup.text.trim().let {
+            if (it.isNotEmpty()) {
+                binding!!.searchRecyclerView.scrollToPosition(0)
+
+            }
+        }
+    }
+
+    companion object {
+        private const val LAST_SEARCH_QUERY: String = "last_search_query"
+        private const val DEFAULT_QUERY = "lipstick"
     }
 
 }
