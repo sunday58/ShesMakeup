9c05fca64ea8c0a6e2894f304dbf1cbdcbc8adac
sundaydavid <sundaydavid989@gmail.com>
2020-09-30 01:41:39 +0100

    finished initializing search fragment

--- app/src/main/java/com/sundaydavid989/shesmakeup/ui/search/SearchFragment.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/search/SearchFragment.kt
@@ -8,11 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
 import com.sundaydavid989.shesmakeup.R
 import com.sundaydavid989.shesmakeup.databinding.SearchFragmentBinding
+import com.sundaydavid989.shesmakeup.ui.adapters.MakeupLoadStateAdapter
 import com.sundaydavid989.shesmakeup.ui.adapters.SearchAdapter
 import com.sundaydavid989.shesmakeup.ui.base.ScopedFragment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -36,6 +39,12 @@ class SearchFragment : ScopedFragment(), KodeinAware {
     private val binding get() = _binding
     private val adapter = SearchAdapter()
 
+    private fun search(query: String) = launch {
+        viewModel.searchMakeup(query).collect {
+            adapter.submitData(it)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -43,12 +52,52 @@ class SearchFragment : ScopedFragment(), KodeinAware {
        _binding = SearchFragmentBinding.inflate(inflater, container, false)
 
 
+        initAdapter()
+        val query = savedInstanceState?.getString(LAST_SEARCH_QUERY) ?: DEFAULT_QUERY
+        search(query)
+        initSearch(query)
+        binding!!.retryButton.setOnClickListener { adapter.retry() }
         return binding?.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this, viewModelFactory).get(SearchViewModel::class.java)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(LAST_SEARCH_QUERY,
+        binding!!.searchMakeup.text.trim().toString())
+    }
+
+    private fun initAdapter(){
+        binding!!.searchRecyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
+            header = MakeupLoadStateAdapter { adapter.retry() },
+            footer = MakeupLoadStateAdapter { adapter.retry() }
+        )
+
+        adapter.addLoadStateListener { loadState ->
+            binding!!.searchRecyclerView.isVisible = loadState.source.refresh is
+                    LoadState.NotLoading
+            binding!!.spinKit.isVisible = loadState.source.refresh is
+                    LoadState.Loading
+            binding!!.retryButton.isVisible = loadState.source.refresh is
+                    LoadState.Error
+
+            val errorState = loadState.source.append as? LoadState.Error
+                ?: loadState.source.append as? LoadState.Error
+                ?: loadState.append as? LoadState.Error
+                ?: loadState.append as? LoadState.Error
+            errorState?.let {
+                showToast("Check Network Connection")
+            }
+        }
+    }
+
+    @Suppress("SameParameterValue")
+    private fun showToast(message: String){
+        Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
     }
 
     private fun initSearch(query: String) {
