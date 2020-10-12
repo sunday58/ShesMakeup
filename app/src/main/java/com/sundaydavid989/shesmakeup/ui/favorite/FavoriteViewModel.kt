a18144155b2e7a75a7fc4fbf60d193de384bed67
sundaydavid <sundaydavid989@gmail.com>
2020-10-12 23:00:21 +0100

    updated favorite viewModel

--- app/src/main/java/com/sundaydavid989/shesmakeup/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/favorite/FavoriteViewModel.kt
@@ -1,7 +1,26 @@
 package com.sundaydavid989.shesmakeup.ui.favorite
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sundaydavid989.shesmakeup.data.db.entity.MakeupItem
+import com.sundaydavid989.shesmakeup.data.repository.MakeupRepository
+import kotlinx.coroutines.*
 
-class FavoriteViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class FavoriteViewModel(
+    private val makeupRepository: MakeupRepository
+) : ViewModel() {
+
+    private val viewModelJob = SupervisorJob()
+    private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.IO)
+
+    fun addFavorite(favoriteItem: MakeupItem){
+        makeupRepository.addFavorite(favoriteItem)
+    }
+
+    fun getFavorite() {
+        viewModelScope.launch {
+            makeupRepository.getFavorite()
+        }
+    }
 }
--- app/src/main/java/com/sundaydavid989/shesmakeup/ui/favorite/FavoriteViewModelFactory.kt
+++ app/src/main/java/com/sundaydavid989/shesmakeup/ui/favorite/FavoriteViewModelFactory.kt
@@ -1,4 +1,24 @@
 package com.sundaydavid989.shesmakeup.ui.favorite
 
-class FavoriteViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.sundaydavid989.shesmakeup.data.repository.MakeupRepository
+import com.sundaydavid989.shesmakeup.ui.home.HomeViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import java.lang.IllegalArgumentException
+
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+@Suppress("UNCHECKED_CAST")
+class FavoriteViewModelFactory(
+    private val makeupRepository: MakeupRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FavoriteViewModel::class.java)){
+            return FavoriteViewModel(makeupRepository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
